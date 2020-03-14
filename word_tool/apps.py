99d8ffaa6b6b370cdab35032af8888509ebfd03b
lucifiash <962862131@qq.com>
2020-03-14 12:01:25 +0800

    添加了源代码文件

---
+++ word_tool/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WordToolConfig(AppConfig):
+    name = 'word_tool'
---
+++ word_tool/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+
+def count(request):
+    uesr_text = request.GET['text']
+    total_count = len(uesr_text)
+    dict_word = {}
+    for word in uesr_text:
+        if word not in dict_word:
+            dict_word[word] = 1
+        else:
+            dict_word[word] += 1
+    sorted_dict =\
+        sorted(dict_word.items(),key=lambda  w : w[1] ,reverse=True)
+
+    return render(request, 'count.html',
+                  {'count':total_count,'text':uesr_text,
+                   'worddict': dict_word,'sorted' : sorted_dict})
